1ad23bad2aaadc77660b8c2ed356a816d4ec5bce
Filips Masolovs <filips.masolovs@gmail.com>
2020-10-04 15:11:47 +0300

    Updates

---
+++ precache-manifest.eda9ca2dad93190811cca467a1cc8903.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce2311305bee5b526a293b8b8e9bf77",
+    "url": "/who-wants-to-be-game/index.html"
+  },
+  {
+    "revision": "f220f16df06aae4d5f3e",
+    "url": "/who-wants-to-be-game/static/css/main.e6393ec6.chunk.css"
+  },
+  {
+    "revision": "13986ee340d7173f78f1",
+    "url": "/who-wants-to-be-game/static/js/2.c1f0705c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/who-wants-to-be-game/static/js/2.c1f0705c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f220f16df06aae4d5f3e",
+    "url": "/who-wants-to-be-game/static/js/main.3b92f9ae.chunk.js"
+  },
+  {
+    "revision": "b1d97a7153030b3534e7",
+    "url": "/who-wants-to-be-game/static/js/runtime-main.33fb4199.js"
+  },
+  {
+    "revision": "635a2b3de729ff4b3563509f270cfe91",
+    "url": "/who-wants-to-be-game/static/media/VC_horizontal.635a2b3d.png"
+  },
+  {
+    "revision": "f3e41a14cb724dc64f80a03701c10db9",
+    "url": "/who-wants-to-be-game/static/media/VC_horizontal_white text.f3e41a14.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/who-wants-to-be-game/precache-manifest.1fa4061de5b32939fd6c881e25d8f903.js"
+  "/who-wants-to-be-game/precache-manifest.eda9ca2dad93190811cca467a1cc8903.js"
 );
 
 self.addEventListener('message', (event) => {
