126559cfefa1be40ad9d5276b22c1760b782fa94
Filips Masolovs <filips.masolovs@gmail.com>
2020-11-02 10:22:50 +0200

    Remove unnecessary, unused code.

--- src/assets/components/game/questions.js
+++ src/assets/components/game/questions.js
@@ -13,180 +13,180 @@ function Questions () {
       question: '1st',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '2nd',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '3rd',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '4th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '5th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '6th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '7th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '8th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '9th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '10th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '11th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '12th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '13th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '14th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '15th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '16th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '17th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '18th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '19th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
     {
       question: '20th',
       answers: [
         {'answer' : 'ABC', 'value' : 1},
-        {'answer' : '04.08.2016', 'value' : 0},
-        {'answer' : '17.08.2017', 'value' : 0},
-        {'answer' : '04.07.2017', 'value' : 0}
+        {'answer' : 'DFG', 'value' : 0},
+        {'answer' : 'HIJ', 'value' : 0},
+        {'answer' : 'KLM', 'value' : 0}
       ]
     },
   ])
