c266ab33167aac4822314668c4cc399ac0e437d3
Filips Masolovs <filips.masolovs@gmail.com>
2021-01-04 08:05:06 +0200

    Update tests.

--- src/assets/components/spotlight.js
+++ src/assets/components/spotlight.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from '../images/VC_horizontal_white text.png'
+import Logo from '../images/VC_horizontal_white.png'
 
 function Spotlight () {
   return (
     <div className='wwvce-spotlight'>
       <img
-        src={logo}
+        src={Logo}
         alt='Who Wants to Be a Visual Composer expert?'
         className='wwvce-spotlight-logo'
       />
